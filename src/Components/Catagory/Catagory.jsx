f5cabea79b9dcf32fbc3a0f9377d00b8328088e2
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-12-28 11:55:59 +0530

    minor bug fix in catagory section

--- src/Components/Catagory/Catagory.jsx
+++ src/Components/Catagory/Catagory.jsx
@@ -4,40 +4,41 @@ import Accordion from "@mui/joy/Accordion";
 import AccordionDetails from "@mui/joy/AccordionDetails";
 import AccordionSummary from "@mui/joy/AccordionSummary";
 import Rating from "@mui/material/Rating";
+import Typography from '@mui/material/Typography';
 import "./Catagory.scss";
 
 const Brand = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
-    <div style={{ display: "flex",alignItems:"start"}}>
-      <input
-        type="checkbox"
-        style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
-      />
-      <label className="label">Mango</label>
+      <div style={{ display: "flex", alignItems: "start" }}>
+        <input
+          type="checkbox"
+          style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
+        />
+        <label className="label">Mango</label>
+      </div>
+      <div style={{ display: "flex", alignItems: "start" }}>
+        <input
+          type="checkbox"
+          style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
+        />
+        <label className="label">H&M</label>
+      </div>
     </div>
-    <div style={{ display: "flex",alignItems:"start"}}>
-      <input
-        type="checkbox"
-        style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
-      />
-      <label className="label">H&M</label>
-    </div>
-  </div>
   );
 };
 
 const PriceRange = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
-      <div style={{ display: "flex",alignItems:"start"}}>
+      <div style={{ display: "flex", alignItems: "start" }}>
         <input
           type="checkbox"
           style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
         />
         <label className="label">Under 500</label>
       </div>
-      <div style={{ display: "flex",alignItems:"start"}}>
+      <div style={{ display: "flex", alignItems: "start" }}>
         <input
           type="checkbox"
           style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
@@ -48,42 +49,18 @@ const PriceRange = () => {
   );
 };
 
-const Ratings = () => {
-  const arr = [5, 4, 3, 2, 1];
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        rowGap: "1rem",
-        justifyContent: "start",
-      }}
-    >
-      {arr.map((e) => (
-        <div
-          style={{ display: "flex", alignItems: "start", columnGap: "1rem" }}
-        >
-          <input
-            type="checkbox"
-            style={{ height: "1.2rem", width: "1.2rem", border: "2px" }}
-          />
-          <Rating name="simple-controlled" value={e} readOnly />
-        </div>
-      ))}
-    </div>
-  );
-};
+
 
 const Catagory = () => {
-  const [expandedArr, setExpanded] = useState([true,true,true]);
-  const handleExpanded = (i) =>() => {
+  const [expandedArr, setExpanded] = useState([true, true, true]);
+  const handleExpanded = (i) => () => {
     const newExanpandArr = [...expandedArr];
-    newExanpandArr[i]= !newExanpandArr[i];
+    newExanpandArr[i] = !newExanpandArr[i];
     setExpanded(newExanpandArr);
   };
   return (
     <div className="catagory">
-      <AccordionGroup disableDivider sx={{ maxWidth: 200 }}>
+      <AccordionGroup disableDivider >
         <Accordion expanded={expandedArr[0]} onChange={handleExpanded(0)} >
           <AccordionSummary >
             BRAND
@@ -92,8 +69,8 @@ const Catagory = () => {
             <Brand />
           </AccordionDetails>
         </Accordion>
-          <hr />
-          <br />
+        <hr />
+        <br />
         <Accordion expanded={expandedArr[1]} onChange={handleExpanded(1)}>
           <AccordionSummary>PRICE RANGE</AccordionSummary>
           <AccordionDetails>
@@ -105,12 +82,19 @@ const Catagory = () => {
         <Accordion expanded={expandedArr[2]} onChange={handleExpanded(2)}>
           <AccordionSummary>RATINGS</AccordionSummary>
           <AccordionDetails>
-            <Ratings />
+            <div style={{display : "flex"}}>
+              <Typography component="legend">Controlled</Typography>
+              <Rating
+                name="simple-controlled"
+                value={5}
+
+              />
+            </div>
           </AccordionDetails>
         </Accordion>
-        
+
       </AccordionGroup>
-    </div>
+    </div >
   );
 };
 
