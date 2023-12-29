90a78cd1509a141b03c45d245a11e3e44b497de0
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-12-29 12:07:54 +0530

    Search bar submit button bug fix

--- src/Components/SearchBar/Searchbar.jsx
+++ src/Components/SearchBar/Searchbar.jsx
@@ -3,34 +3,34 @@ import { useNavigate } from 'react-router-dom';  // Use useNavigate instead of u
 import './Searchbar.scss';
 
 const Searchbar = ({ handleDisplay, displayNav }) => {
-  const navigate = useNavigate();  // Use useNavigate instead of useHistory
-  const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();  // Use useNavigate instead of useHistory
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform any search-related logic here if needed
-    // ...
-    
-    // Redirect to "/home"
-    navigate('/home');
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Perform any search-related logic here if needed
+        // ...
 
-  return (
-    <div className='searchbar' style={{ display: displayNav === true ? 'block' : 'none' }}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Search'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onClick={handleDisplay}  
-        />
-        <button type='submit'>
-          <i className='fa-solid fa-magnifying-glass'></i>
-        </button>
-      </form>
-    </div>
-  );
+        // Redirect to "/home"
+        navigate('/home');
+    };
+
+    return (
+        <div className='searchbar' style={{ display: displayNav === true ? 'block' : 'none' }}>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Search'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onClick={handleDisplay}
+                />
+
+                <i type='submit' className='fa-solid fa-magnifying-glass'></i>
+
+            </form>
+        </div>
+    );
 };
 
 export default Searchbar;
