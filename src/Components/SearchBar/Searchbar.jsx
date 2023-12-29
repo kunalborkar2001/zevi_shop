8caeb9673c25aaa968c30c2e909b20941ff870dc
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-12-29 12:03:02 +0530

    Added Search redirection

--- src/Components/SearchBar/Searchbar.jsx
+++ src/Components/SearchBar/Searchbar.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';  // Use useNavigate instead of useHistory
+import './Searchbar.scss';
 
+const Searchbar = ({ handleDisplay, displayNav }) => {
+  const navigate = useNavigate();  // Use useNavigate instead of useHistory
+  const [searchTerm, setSearchTerm] = useState('');
 
-import './Searchbar.scss'
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform any search-related logic here if needed
+    // ...
+    
+    // Redirect to "/home"
+    navigate('/home');
+  };
 
-const Searchbar = ({ handleDisplay , displayNav}) => {
-    // console.log(displayNav);
-    return (
-        <div className='searchbar' style={{ display: displayNav === true ? "block" : "none" }}>
-            <input type="text" placeholder='Search' onClick={handleDisplay} />
-            <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
-    );
+  return (
+    <div className='searchbar' style={{ display: displayNav === true ? 'block' : 'none' }}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onClick={handleDisplay}  
+        />
+        <button type='submit'>
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Searchbar;
